c5f48d551c7e4dc0542e84935e333546c97a63f0
rxs7986 <rachanasagar0801@gmail.com>
2023-06-02 12:39:33 -0500

    some kind of commit message

--- Day3/Java/Task2/src/Main.java
+++ Day3/Java/Task2/src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Product product = new Product();
+        product.setPrice(10.5); // Set the price for a single quantity
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        double price = product.getPrice(); // Get the price for a single quantity
+        System.out.println("Price for a single quantity: " + price);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        int quantity = 5;
+        double totalPrice = product.getPrice(quantity); // Get the price for a specified quantity
+        System.out.println("Total price for " + quantity + " quantities: " + totalPrice);
+
     }
 }
