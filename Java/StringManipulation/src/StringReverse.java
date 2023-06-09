c109b81d3df32cfbbcd232829a664fe2cb518d26
rxs7986 <rachanasagar0801@gmail.com>
2023-06-09 00:01:14 -0500

    added js on 06/08

---
+++ Java/StringManipulation/src/StringReverse.java
@@ -0,0 +1,18 @@
+public class StringReverse
+{
+    public static void main(String[] args)
+{
+    String str = "Java J2EE Reverse Me";
+    String revstr = "";
+
+    String str3 = str.split(" ");
+    for(int i = 0 ; i < n ; i++)
+    {
+        System.out.println(str3[i]);
+    }
+
+
+
+
+}
+}
